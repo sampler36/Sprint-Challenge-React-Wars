abe81d478ce80460ffc1efdee5447e6452289617
Talent Antonio <antonio.talenty@gmail.com>
2019-02-22 11:28:23 +0000

    Character States

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -7,12 +7,19 @@ export function CharacterList({ starwarsChars, getCharInfo }) {
         starwarsChars.map(character =>
           <div key={character.name}>
             <h2>{character.name}</h2>
+            <p>Created: {character.created}</p>
             <p>Birth year: {character.birth_year}</p>
+            <p>Eye color: {character.eye_color}</p>
             <p>Gender: {character.gender}</p>
+            <p>Mass: {character.mass}</p>
+            <p>Skin color: {character.skin_color}</p>
+            <p>height: {character.height}</p>
+            <p>hair color: {character.haircolor}</p>
             <p>Species: {getCharInfo(character.species)}</p>
+            <p>Edited: {character.edited}</p>
           </div>
         )
-      }
+      } 
    </div>
   );
 }
